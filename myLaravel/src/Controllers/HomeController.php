a9b281964a034d9b2d81830c7b0d9d4e59ac3427
Oleksandr Nykytin <keeper@ninydev.com>
2022-07-06 18:47:54 +0300

    Begin

---
+++ myLaravel/src/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController
+{
+    /**
+     * Построить главную страницу сайта
+     */
+    public function index(){
+        echo "Work";
+    }
+
+}
---
+++ myLaravel/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ninydev/mylaravel',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1f0d5894747a3607b880d69001dae986a0316c08',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ninydev/mylaravel' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1f0d5894747a3607b880d69001dae986a0316c08',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '375d77cef7c96fded38a21c90becef6f8c2a96ca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
